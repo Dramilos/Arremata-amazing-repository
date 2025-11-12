b536b191cbbafaa1a2d4dc8199d91c50fe01e071
MtBalbine <matheusbalbine@gmail.com>
2025-11-12 08:51:04 -0300

    Ajuste

--- src/frontend/scripts/Perfil_Prestador.js
+++ src/frontend/scripts/Perfil_Prestador.js
@@ -1,20 +1,4 @@
 
-/*
-// ===== Habilitar edição de campos =====
-const editables = document.querySelectorAll('.editable');
-let editing = false;
-*/
-/*
-// ===== Sistema de estrelas =====
-const rating = document.getElementById('rating');
-rating.addEventListener('click', (e) => {
-  const rect = rating.getBoundingClientRect();
-  const clickX = e.clientX - rect.left;
-  const starWidth = rect.width / 5;
-  const selected = Math.ceil(clickX / starWidth);
-  rating.textContent = "★★★★★".slice(0, selected) + "☆☆☆☆☆".slice(selected, 5);
-});*/
-
 // ===== Upload e troca de imagem de perfil =====
 const profileImg = document.getElementById('profileImg');
 const uploadImg = document.getElementById('uploadImg');
